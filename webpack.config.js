fb3f72084a702f95f617ae9aa04f3ee39c476ce3
cornelia bodea <cornelia.bodea@evozon.com>
2018-10-18 14:02:12 +0300

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  entry: {
+    app: './src/index.jsx',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {minimize: true}
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
