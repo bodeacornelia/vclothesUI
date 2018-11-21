55e575a1fad93e48e1e3701e38165f949a71e961
cornelia bodea <cornelia.bodea@evozon.com>
2018-11-21 17:44:13 +0200

    minor changes

--- src/modules/Auth/store/actions.ts
+++ src/modules/Auth/store/actions.ts
@@ -3,9 +3,9 @@ import Auth from '../../../utils/AuthenticationUtils';
 import { navigateTo } from '../../Menu/store/actions';
 
 export const login = (payload: any) => (dispatch: any) => {
-  // AuthService.login(payload).then((token: string) => { Auth.setToken(token); })
+  AuthService.login(payload).then((token: string) => { Auth.setToken(token); })
 
-  // if(Auth.getToken()) {
-  dispatch(navigateTo({ path: '/' }))
-  // }
+  if (Auth.getToken()) {
+    dispatch(navigateTo({ path: '/' }))
+  }
 };
